712831e570806e5d5a97305b34e9295abd76851c
Vladivostok79 <inga.p280@gmail.com>
2025-05-16 15:01:31 +0000

    Fri, May 16, 2025, 10:01 PM +07:00

--- bands/models.py
+++ bands/models.py
@@ -5,5 +5,5 @@ class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth = models.DateField()
-    def __str__(self):
-        return self.first_name
+    def _str_(self):
+         return f"{self.first_name} {self.last_name}"
